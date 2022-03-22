a7c4a959aec7f95519809ab98c1312e8aac271c2
box-monkey <jr.gasparin15@gmail.com>
2022-03-22 01:53:26 -0400

    added login functionality and cleaned up with comments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 // Page imports.
 const { AuthenticationError } = require('apollo-server-express');
-const { signToken } = require("../utils/auth");
+const { signToken } = require('../utils/auth');
 
 // Queries - obtains information.
 const resolvers = {
@@ -15,13 +15,34 @@ const resolvers = {
   },
   // Mutations - creates, updates or deletes.
   Mutation: {
+    // New user logic.
     addUser: async (parent, args) => {
       const user = await User.create(args);
       const token = signToken(user);
 
+      // Returns created info.
       return { user, token };
     },
-    login: async () => {}
+    // Login logic.
+    login: async (email, password) => {
+      const user = await User.findOne({ email });
+
+      // Throws an error if user input is incorrect or no user has been created with attempted login credentials
+      if (!user) {
+        throw new AuthenticationError('No user found.');
+      }
+
+      const loginPassword = await user.correctPassword(password);
+
+      // Throws an error if password is incorrect
+      if (!loginPassword) {
+        throw new AuthenticationError('Password is invalid.');
+      }
+
+      const token = signToken(user);
+      // Returns the login info.
+      return { token, user };
+    }
   }
 };
 
