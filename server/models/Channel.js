df482ad0e6b3d60a3a22b9671272d96e026e9106
Kelerik <vincent.ng39@gmail.com>
2022-03-23 18:04:05 -0400

    case fix

--- server/models/Channel.js
+++ server/models/Channel.js
@@ -1,12 +1,12 @@
 const { Schema } = require('mongoose');
-const messageSchema = require('./Message');
+const MessageSchema = require('./Message');
 
 const ChannelSchema = new Schema({
   name: {
     type: String,
     required: true
   },
-  messages: [messageSchema],
+  messages: [MessageSchema],
   users: [
     {
       type: Schema.Types.ObjectId,
