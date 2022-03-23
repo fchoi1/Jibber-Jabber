e42b3855eb130427c225bd2d34e8219754c94cfc
Kelerik <vincent.ng39@gmail.com>
2022-03-23 17:47:58 -0400

    Message typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,16 +1,19 @@
-
-const {gql} = require("apollo-server-express")
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+  }
 
-    type User{
-        _id: ID
-    
-    
-    }
-    
-    type Query {
-        me: User
-    } 
-`
-module.exports = typeDefs
+  type Query {
+    me: User
+  }
+
+  type Message {
+    _id: ID
+    user: User
+    textContent: String
+    timeStamp: Int
+  }
+`;
+module.exports = typeDefs;
