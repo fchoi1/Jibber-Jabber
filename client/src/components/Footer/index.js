d9d5333152cc094f2ddfce779ba146aec32617b9
Yashar Js <Yashar@Icys-MacBook-Pro.local>
2022-03-26 12:29:10 -0400

    error fix

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { SocialIcon } from 'react-social-icons';
 function DisplayFooter(){
     return (
-        <div className=" bg-dark text-center d-flex footer justify-content-center" style={{"margin-top":"auto"}}>
+        <div className=" bg-dark text-center d-flex footer justify-content-center" style={{"marginTop":"auto"}}>
             dsdsd
             <div className="m-2"><SocialIcon url="https://twitter.com/karansodhi"/></div>
             <div className="m-2"><SocialIcon url="https://facebook.com/karan.sodhi" /></div>
