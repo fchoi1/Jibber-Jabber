be269e0680aa833850fa87268557f588dc05a8cf
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-03-28 02:03:56 -0400

    showing recent chats

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,6 +5,7 @@ const {signToken,authMiddleware} = require("../utils/auth")
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
+            console.log(context.user)
             if (context.user) {
               const userData = await User.findOne({ _id: context.user._id })
               .populate({
