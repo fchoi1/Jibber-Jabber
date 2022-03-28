1d3de66eb3f3a681dbb73bd3ab11d78616bad10c
Huzaifa Mohammed <huzirizwan@gmail.com>
2022-03-28 02:12:27 -0400

    channel page styling

--- client/src/pages/Channel.js
+++ client/src/pages/Channel.js
@@ -33,17 +33,23 @@ const Channel = (props) => {
         ))}
       </div>
       {/* map through all messages to show their content */}
-      <div>
+      
+
+      <div className='chat-box'>
+       
+
         {messages.map((message) => (
-          <div key={message._id}>
-            <Message
+          <div key={message._id} >
+            <Message 
               username={message.username}
               textContent={message.textContent}
               timeStamp={message.timeStamp}
-            />
+              />
           </div>
         ))}
+       
       </div>
+      
     </>
   );
 };
