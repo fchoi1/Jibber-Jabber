b178dd7e1ee5b2529248e9c1f97572754c650134
Yashar <yasharjs@gmail.com>
2022-03-28 20:20:36 -0400

    welcomepage

--- client/src/components/WelcomePage/index.js
+++ client/src/components/WelcomePage/index.js
@@ -1,40 +1,33 @@
-import React from 'react'
-import Auth from '../../utils/auth'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
 
-import './welcomepage.css'
+import './welcomepage.css';
 
-import { useQuery } from '@apollo/client'
-import {QUERY_ME} from '../../utils/queries'
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../../utils/queries';
 
 export default function WelcomePage() {
-    const loggedIn = Auth.loggedIn();
-    console.log("Logged in: ",loggedIn)
+  const loggedIn = Auth.loggedIn();
+  console.log('Logged in: ', loggedIn);
 
-    const {loading, data} = useQuery(QUERY_ME);
-    console.log("User Data: ",data)
-    if(loading){
-        console.log('Loading')
-        return <div>Loading...</div>
-      
-    }
-  
+  const { loading, data } = useQuery(QUERY_ME);
+  console.log('User Data: ', data);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-
-
-    return (
-        <div className="welcome-div">
-            <h1>Welcome Page</h1>
-        {loggedIn && data ? (
-            <h1>{data.me.username}</h1>
-        ): null}
-            <button>
+  return (
+    <div className="welcome-div">
+      <h1>Welcome</h1>
+      {loggedIn && data ? <h1>{data.me.username}</h1> : null}
+      <button>
         {loggedIn ? (
-            <Link to="/dashboard">Dashboard</Link>
+          <Link to="/dashboard">Dashboard</Link>
         ) : (
-            <Link to="/signup">Signup / Login</Link>
+          <Link to="/signup">Signup / Login</Link>
         )}
-            </button>
-        </div>
-    )
+      </button>
+    </div>
+  );
 }
