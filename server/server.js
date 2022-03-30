b39c485525f2254ed36c1da9c06480f1044eafc6
fchoi1 <masterfabster2010@hotmail.com>
2022-03-30 14:12:32 -0400

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -68,9 +68,9 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once('open', () => {
   // app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
